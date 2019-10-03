6fdcb622bb9719439169bd8f0ecf171f3ee5b96b
zgy <2208396564@qq.com>
2019-10-03 18:18:29 +0800

    new snakeWeb

--- src/views/product/placeOrder.vue
+++ src/views/product/placeOrder.vue
@@ -2,7 +2,21 @@
   <div class="place-order">
     <commonTopTwo></commonTopTwo>
     <div class="order-container width-1200">
+      <div class="order-left">
+        <h3>订单信息</h3>
+        <div>
+          <div>
+            <span>商品名称：</span>
+            <span></span>
+          </div>
+          <div>
+            <span>充值金额：</span>
+          </div>
+        </div>
+      </div>
+      <div class="order-right">
 
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +48,19 @@
     .order-container{
       border: 1px solid #dbdbdb;
       margin: 30px auto;
+      .order-left{
+        width: 380px;
+        h3{
+          margin: 0;
+          background-color: #f3f3f3;
+          color: #333;
+          font-size: 14px;
+          height: 44px;
+          line-height: 44px;
+          border-bottom: 1px solid #dbdbdb;
+          text-indent: 25px;
+        }
+      }
     }
   }
 </style>
