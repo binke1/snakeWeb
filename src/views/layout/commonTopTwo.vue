3d9e3c6635eb74f72ec6399651c29858a3854c11
binke1 <1426683814@qq.com>
2019-10-03 20:50:52 +0800

    111

--- src/views/layout/commonTopTwo.vue
+++ src/views/layout/commonTopTwo.vue
@@ -2,8 +2,8 @@
     <div class="common-top-two">
       <div class="width-1200 user-info">
         <span>
-          <span>首页</span>
-          <span>
+          <span @click="$router.push('/')">首页</span>
+          <span @click="$router.push('/personalCenter/membersArea')" v-if="userName">
             您好！{{userName}}
           </span>
         </span>
@@ -41,7 +41,7 @@
       justify-content: space-between;
       span{
         font-size: 12px;
-        color: #999;
+        color: #bbb;
         cursor: pointer;
         span{
           margin-left: 20px;
--- src/views/product/placeOrder.vue
+++ src/views/product/placeOrder.vue
@@ -7,16 +7,51 @@
         <div>
           <div>
             <span>商品名称：</span>
-            <span></span>
+            <span>订单标题</span>
           </div>
           <div>
             <span>充值金额：</span>
+            <span>100元 * 1</span>
           </div>
+          <div>
+            <span>支付方式：</span>
+            <span>支持任何支付方式</span>
+          </div>
+          <div>
+            <span>小计：</span>
+            <span>C$23.61CAD</span>
+          </div>
+          <div>
+            <span>税：</span>
+            <span>C$5.00CAD</span>
+          </div>
+          <div>
+            <span>付款金额：</span>
+            <span>C$25.12CAD</span>
+           </div>
         </div>
       </div>
       <div class="order-right">
-
+        <p class="order-right-sure">请确认订单信息</p>
+        <p class="sure-hint">登录代充，请备注密码或联系客服</p>
+        <div class="user-apple">
+          <span>充值 APPLE ID：</span>
+          <input type="text">
+          <span>*</span>
+        </div>
+        <div class="order-comment">
+          <span>订单备注：</span>
+          <textarea placeholder="请输入备注信息..." cols="30" rows="5"></textarea>
+        </div>
       </div>
+    </div>
+    <div class="pay-method">
+      <span>付款方式</span>
+       <div>支付宝</div>
+       <div>微信</div>
+    </div>
+    <div class="submit-order width-1200">
+      <button>提交订单</button>
     </div>
   </div>
 </template>
@@ -47,9 +82,11 @@
   .place-order{
     .order-container{
       border: 1px solid #dbdbdb;
-      margin: 30px auto;
+      margin: 30px auto 15px;
+      display: flex;
       .order-left{
         width: 380px;
+        border-right: 1px solid #dbdbdb;
         h3{
           margin: 0;
           background-color: #f3f3f3;
@@ -60,6 +97,95 @@
           border-bottom: 1px solid #dbdbdb;
           text-indent: 25px;
         }
+        >div{
+          padding: 0 25px;
+          div{
+            font-size: 12px;
+            margin: 15px 0;
+            color: #888;
+            >span{
+              &:first-child{
+                display: inline-block;
+                width: 60px;
+                text-align: right;
+              }
+            }
+          }
+        }
+      }
+      .order-right{
+        padding-left: 90px;
+        .order-right-sure{
+          font-size: 20px;
+          color: #333;
+        }
+        .sure-hint{
+          color: #fe6f00;
+          font-size: 12px;
+          margin: 10px 0;
+        }
+        div{
+          font-size: 12px;
+          color: #777;
+          textarea{
+            border: 1px solid #bcbcbc;
+            width: 480px;
+            resize: none;
+            border-radius: 3px;
+            padding: 2px 14px;
+          }
+          >span{
+            display: inline-block;
+            width: 100px;
+          }
+        }
+        .order-comment{
+          display: flex;
+          align-items: flex-start;
+          margin: 20px 0 10px;
+        }
+        .user-apple{
+          input{
+            width: 290px;
+            height: 26px;
+            border: 1px solid #bcbcbc;
+            border-radius: 6px;
+          }
+          span{
+            &:last-child{
+              color: #f00;
+            }
+          }
+        }
+      }
+    }
+    .pay-method{
+      width: 1104px;
+      margin: auto;
+      border: 1px solid #dbdbdb;
+      padding: 16px 48px;
+      display: flex;
+      align-items: center;
+      span{
+        color: #777;
+        font-size: 12px;
+      }
+      >div{
+        margin-left: 30px;
+      }
+    }
+    .submit-order{
+      margin: 20px auto 30px;
+      button{
+        width: 164px;
+        color: #fff;
+        cursor: pointer;
+        outline: none;
+        border: none;
+        font-size: 18px;
+        border-radius: 2px;
+        background-color: #e9322e;
+        height: 44px;
       }
     }
   }
